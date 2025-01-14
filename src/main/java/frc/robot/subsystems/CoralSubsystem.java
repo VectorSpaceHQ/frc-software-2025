096b32f1ccf0aa27c568f19f59cdf4ca6ac3af12
Holden Hughes <holdenhughes88@gmail.com>
2025-01-13 19:45:27 -0500

    Add CoralSubsystem

---
+++ src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** 
+ * The subsystem controlling the release of currently loaded coral
+ */
+public class CoralSubsystem extends SubsystemBase{
+    public CoralSubsystem() {
+
+    }
+
+    @Override
+    public void periodic() {
+        // Most likely useless, but good template for Josiah
+    }
+
+    @Override
+    public void simulationPeriodic() {
+      // This method will be called once per scheduler run during simulation
+    }
+}
+
