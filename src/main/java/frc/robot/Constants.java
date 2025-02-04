07d8ebdf4e396f4e4b6a778e229a644123c6d072
Holden Hughes <holdenhughes88@gmail.com>
2025-02-03 19:06:44 -0500

    Refactored for Krakens, Need actual testing to determine subsystem efficacy.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -74,6 +74,10 @@ public final class Constants {
   public static final class CANIDs {
     public static final int kCoralSubsystemLeft = 11;
     public static final int kCoralSubsystemRight = 12;
+    public static final int kDriveSubsystemFrontRight = 13;
+    public static final int kDriveSubsystemFrontLeft = 14;
+    public static final int kDriveSubsystemRearRight = 15;
+    public static final int kDriveSubsystemRearLeft = 16;
   }
 
   public static final class AutoConstants {
