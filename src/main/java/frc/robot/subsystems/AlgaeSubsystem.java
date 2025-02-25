442c2b0d404579f9a21047189a5b4d2ecf821ee3
Holden Hughes <holdenhughes88@gmail.com>
2025-02-25 17:16:23 -0500

    Fixed Duplicate Lines

--- src/main/java/frc/robot/subsystems/AlgaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeSubsystem.java
@@ -3,12 +3,9 @@ package frc.robot.subsystems;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -20,14 +17,10 @@ public class AlgaeSubsystem extends SubsystemBase {
     private double speed = 0;
     private final SparkMax motor_left = new SparkMax(CANIDs.kAlgaeSubsystemLeft, MotorType.kBrushless);
     private final SparkMax motor_right = new SparkMax(CANIDs.kAlgaeSubsystemRight, MotorType.kBrushless);
-    private SparkMaxConfig config = new SparkMaxConfig();
     private SparkMaxConfig defaultconfig = new SparkMaxConfig();
     private SparkMaxConfig config = new SparkMaxConfig();
-    private SparkMaxConfig defaultconfig = new SparkMaxConfig();
     private final DigitalInput l_Left = new DigitalInput(DigitalInputPorts.kAlgaeSubsystemLeft);
     private final DigitalInput l_Right = new DigitalInput(DigitalInputPorts.kAlgaeSubsystemRight);
-    private boolean limitSwitchLeft = l_Left.get();
-    private boolean limitSwitchRight = l_Right.get();
     private boolean limitSwitchLeft = l_Left.get();
     private boolean limitSwitchRight = l_Right.get();
 
