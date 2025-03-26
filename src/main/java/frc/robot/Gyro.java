5038209a60efef3158621e8ccc5a4d880b93ee01
Holden Hughes <holdenhughes88@gmail.com>
2025-03-26 15:33:16 -0400

    Added Interface Functionality Back

--- src/main/java/frc/robot/Gyro.java
+++ src/main/java/frc/robot/Gyro.java
@@ -1,5 +1,9 @@
 package frc.robot;
+import edu.wpi.first.math.geometry.Rotation2d;
 
 public interface Gyro {
-
+  public Rotation2d getRotation2d();
+  public double getRate();
+  public void reset();
+  public void DisplayIMUData();
 }
